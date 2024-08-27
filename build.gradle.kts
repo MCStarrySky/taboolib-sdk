9885604b399fbeb1a84b89e5c08c8c0b0821e9b0
米咔噜 <m@taboo.sh>
2024-08-27 08:34:58 +0800

    Update TabooLib

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,19 +4,29 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     java
-    id("io.izzel.taboolib") version "2.0.11"
+    id("io.izzel.taboolib") version "2.0.13"
     id("org.jetbrains.kotlin.jvm") version "1.8.22"
 }
 
 taboolib {
     env {
         // 安装模块
-        install(UNIVERSAL, BUKKIT)
+        install(
+            "minecraft-chat",
+            "basic-configuration",
+            "minecraft-i18n",
+            "minecraft-command-helper",
+            "platform-bukkit"
+        )
+        repoTabooLib = "http://mcstarrysky.com:8081/repository/releases/"
     }
-    version { taboolib = "6.1.2-beta10" }
+    version { taboolib = "6.2.0-beta20-dev" }
 }
 
 repositories {
+    maven("http://mcstarrysky.com:8081/repository/releases/") {
+        isAllowInsecureProtocol = true
+    }
     mavenCentral()
 }
 
